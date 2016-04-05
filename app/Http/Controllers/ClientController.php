ba3c26213eef195649474214ecfe7731caf7c5b8
Marcelo Ribeiro <eng.msilva@yahoo.com.br>
2016-04-05 16:19:37 -0400

    - Criado o arquivo Controller da entidade Client(ClientController); - Configurado o arquivo ClientController a funcao all para listar todos os   dados da tabela Client; - Configurado a rota no arquivo routes para apontar a url /client para a   funcao index do Controller ClientController.

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        return \CodeProject\Client::all();
+    }
+}
