f4c4a259cf717d1d299972cd699fac765ed83497
Marcelo Ribeiro <eng.msilva@yahoo.com.br>
2016-04-05 22:06:28 -0400

    - Configurado a funcao show no arquivo ClientController para retornar   apenas um registro da tabela Client; - Configurado a rota no arquivo routes para apontar a url get /client/{id}   para a funcao show do Controller ClientController; - Configurado a funcao destroy no arquivo ClientController para apagar um registro da tabela Client;   - Configurado a rota no arquivo routes para apontar a url delete /client/{id}     para a funcao destroy do Controller ClientController;

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -9,13 +9,30 @@ use CodeProject\Http\Requests;
 
 class ClientController extends Controller
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function index()
     {
         return \CodeProject\Client::all();
     }
 
+    /**
+     * @param Request $request
+     * @return static
+     */
     public function store(Request $request)
     {
         return Client::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return Client::find($id);
+    }
+
+    public function destroy($id)
+    {
+        return Client::find($id)->delete();
     }
 }
